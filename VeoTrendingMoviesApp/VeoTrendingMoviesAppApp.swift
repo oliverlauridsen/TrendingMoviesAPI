82e141beacc25eeb5c2516b969052cf3aca372d2
Oliver Lauridsen <olidude2.0@gmail.com>
2022-09-29 14:38:26 +0200

    Initial Commit

---
+++ VeoTrendingMoviesApp/VeoTrendingMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeoTrendingMoviesAppApp.swift
+//  VeoTrendingMoviesApp
+//
+//  Created by Oliver Lauridsen on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct VeoTrendingMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
